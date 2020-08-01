ee9eb4c819e29ca4ed671ded61b512d74739ba9b
Zach Philipchook <z.pilipchuk@gmail.com>
2020-08-01 18:04:27 +0300

    Initial version

---
+++ README.md
@@ -0,0 +1,4 @@
+**password-deriver** is a tool to derive passwords from a master password in a deterministic way. It is indended for my personal use, so breaking changes are possible and even likely.
+
+The key derivation method is the same one that's used in the [**lesspass**](https://github.com/lesspass/lesspass).
+The password formatting is also currently **lesspass**-compatible, but that's a subject to change.
---
+++ src/password_deriver/__main__.py
@@ -0,0 +1,69 @@
+import string
+import argparse
+import subprocess
+import getpass
+import hashlib
+
+
+def derive_secret_key(master_password, domain, username, counter):
+    salt = f"{domain}{username}{counter:x}".encode("utf-8")
+    return hashlib.pbkdf2_hmac(
+        "sha256", master_password, salt=salt, iterations=100_000, dklen=32
+    )
+
+
+def format_password_lesspass(secret_key):
+    main_alphabet = (
+        string.ascii_lowercase
+        + string.ascii_uppercase
+        + string.digits
+        + string.punctuation
+    )
+    seed = int.from_bytes(secret_key, byteorder="big")
+
+    password = []
+    for _ in range(12):
+        seed, index = divmod(seed, len(main_alphabet))
+        password.append(main_alphabet[index])
+
+    extra_chars = []
+    for alphabet in [
+        string.ascii_lowercase,
+        string.ascii_uppercase,
+        string.digits,
+        string.punctuation,
+    ]:
+        seed, index = divmod(seed, len(alphabet))
+        extra_chars.append(alphabet[index])
+
+    for extra_char in extra_chars:
+        seed, index = divmod(seed, len(password))
+        password.insert(index, extra_char)
+
+    return "".join(password)
+
+
+def write_to_clipboard(data):
+    process = subprocess.Popen("pbcopy", stdin=subprocess.PIPE)
+    process.communicate(data.encode("ascii"))
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("domain")
+    parser.add_argument("--user", default="")
+    parser.add_argument("--counter", type=int, default=1)
+    args = parser.parse_args()
+
+    master_password = getpass.getpass("Enter the master password: ").encode("utf-8")
+
+    secret_key = derive_secret_key(
+        master_password, args.domain, args.user, args.counter
+    )
+    password = format_password_lesspass(secret_key)
+
+    write_to_clipboard(password)
+
+
+if __name__ == "__main__":
+    main()
