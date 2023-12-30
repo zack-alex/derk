8949446894a44913eacedb8538b3cee66e42e025
Zach Philipchook <z.pilipchuk@gmail.com>
2023-12-30 20:43:18 +0200

    nixify

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A Nix flake for the password-deriver Python package";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ ];
+        };
+        pythonEnv = pkgs.python3.withPackages (ps: [ ]);
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [ pythonEnv pkgs.python3Packages.setuptools ];
+        };
+
+        packages.default = pkgs.python3Packages.buildPythonPackage {
+          pname = "password-deriver";
+          version = "0.0.1";
+          src = self;
+          propagatedBuildInputs = [ pythonEnv ];
+        };
+      }
+    );
+}
+
