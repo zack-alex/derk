d79b2d7ef3cad129b19b030fc92913d21fb36c92
Zach Philipchook <z.pilipchuk@gmail.com>
2024-06-23 12:15:12 +0300

    Move test file

--- tests/password_deriver/algorithms.py
+++ tests/password_deriver/algorithms.py
@@ -6,14 +6,14 @@ from password_deriver.algorithms import derive_and_format
 
 
 def test_algorithms():
-    with open("tests/password_deriver/data.json", "r") as f:
+    with open("test-data.json", "r") as f:
         data = json.load(f)
     fixed_data = []
     for spec, _ in data:
         full_spec = dict(spec, domain="test_domain", username="test_username")
         res = derive_and_format(b"test_master_password", full_spec)
         fixed_data.append([spec, res])
-    with open("tests/password_deriver/data.json", "w") as f:
+    with open("test-data.json", "w") as f:
         json.dump(fixed_data, f, indent=2)
 
 
